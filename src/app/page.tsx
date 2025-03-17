fe748879f0901622ee1f2a6a1ee75abae119f2fe
0xamitr <0xamitr@gmail.com>
2025-03-17 15:01:30 +0530

    align center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <main >
+      <main className="flex flex-col items-center max-width: var(--container-7xl)">
         <h1>check;</h1>
       </main>
      
